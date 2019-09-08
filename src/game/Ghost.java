afa1a6d8bf9fd49eb9c2a580aba643d8e8253b0b
Luc Huynh <37423125+s3724706@users.noreply.github.com>
2019-09-08 16:59:29 +1000

    Update Ghost.java

--- src/game/Ghost.java
+++ src/game/Ghost.java
@@ -1,5 +1,29 @@
 package game;
 
-public class Ghost {
+public class GhostData {
+
+	private	int posX;
+	private int posY;
+	private String direction;
+	
+	public int posX() {
+		return posX;
+	}
+	
+	public int posY() {
+		return posY;
+	}
+	
+	public String getDirection() {
+		return direction;
+	}
+	
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+	
+	//public boolean frozen() {
+	//	if 
+	// }
 
 }
