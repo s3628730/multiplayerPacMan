b2b1503ea9d69348ec649efedb099d35b52483f9
s3628730 <37060647+s3628730@users.noreply.github.com>
2019-09-08 10:39:28 +1000

    Update GameState.java
    added an update method, still somewhat pseudo code.

--- src/game/GameState.java
+++ src/game/GameState.java
@@ -1,6 +1,38 @@
 package game;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import game.enumeration.GameStates;
+import game.enumeration.PlayerNum;
+
 public class GameState {
 
-	//private Map<PlayerNum, player> players
+	/* public void update() {
+		switch (currentState) {
+			
+			case PLAYING:
+				updateMainGame();
+				if (PACMAN_DEATH) {
+					currentState = PACMAN_DEATH;
+				}
+			break;
+		
+			case GAME_OVER:
+				updateGameOver();
+				currentState = GAME_OVER;
+			break;
+		
+			case PACMAN_DEATH:
+				PACMAN_LIVES -= 1;
+				if (PACMAN_LIVES != 0) {
+				restartMainGame();
+				} else {
+					currentState = GAME_OVER;
+				}
+			break;
+		}
+	} */
+
 }
+
