a99b71cc7245ab38ca341c5d2305d99994e566d4
s3628730 <37060647+s3628730@users.noreply.github.com>
2019-09-08 10:44:10 +1000

    Update GameEngine.java
    first update for Game Engine, includes update method, and movement methods on X and Y axis

--- src/game/GameEngine.java
+++ src/game/GameEngine.java
@@ -4,7 +4,39 @@ import game.interfaces.GameEngineInterface;
 
 public class GameEngine implements GameEngineInterface {
 
-	public static void updategameState(GameState gameState) {
-		//TODO update game state
+	GameMap map = new GameMap();
+	Player player = new Player();
+	Ghost ghost = new Ghost();
+	
+	
+	//update game state
+	public void updategameState(GameState gameState) {
+		gameState.update();
 	}
-}
+	
+	
+	public void movementUpdateX() {
+		/* for (GameObject gameObject : gameObjects) {
+		 * 	if (gameObject.getPosX > 1 && gameObject.getPosX < 10 && curDirection == RIGHT) {
+		 * 		gameObject.setPosX(getPosX += 1)
+		 * 		}
+		 *  if else (gameObject.getPosX > 1 && gameObject.getPosX < 10 && curDirection == LEFT) {
+		 * 		gameObject.setPosX(getPosX -= 1)
+		 * 		}
+		 * }
+		 * 
+		 */
+	}
+	
+	public void movementUpdateY() {
+		/* for (GameObject gameObject : gameObjects) {
+		 * 	if (gameObject.getPosY > 1 && gameObject.getPosY < 10 && curDirection == DOWN) {
+		 * 		gameObject.setPosY(getPosY += 1)
+		 * 		}
+		 *  if else (gameObject.getPosY > 1 && gameObject.getPosY < 10 && curDirection == UP) {
+		 * 		gameObject.setPosY(getPosY -= 1)
+		 * 		}
+		 * }
+		 * 
+		 */
+	}
